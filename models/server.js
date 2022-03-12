48032be38c98e6489183d9b1dcab10716eb80bb1
Guillermo Marcano <carlosgmarcano@gmail.com>
2022-03-11 22:26:59 -0500

    First Commit Linux

--- models/server.js
+++ models/server.js
@@ -1,51 +1,49 @@
-const express = require('express');
-const cors = require('cors');
-const { dbConnection } = require('../DB/config');
+const express = require("express");
+const cors = require("cors");
+const { dbConnection } = require("../DB/config");
 
 class Server {
-    constructor() {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.usuariosPath = "/api/usuarios";
+    this.authPath = "/api/auth";
 
-        this.app = express();
-        this.port = process.env.PORT;
-        this.usuariosPath = '/api/usuarios';
-        this.authPath = '/api/auth';
+    //Conectar DB
+    this.conectarDB();
 
-        //Conectar DB
-        this.conectarDB();
+    //Middlewares
+    this.middlewares();
 
-        //Middlewares
-        this.middlewares();
+    //Rutas de Aplicacion
+    this.routes();
+  }
 
-        //Rutas de Aplicacion
-        this.routes();
-    }
+  async conectarDB() {
+    await dbConnection();
+  }
 
-    async conectarDB() {
-        await dbConnection();
-    }
+  middlewares() {
+    //Directorio publico
+    this.app.use(express.static("public"));
 
-    middlewares() {
+    //CORS
+    this.app.use(cors());
 
-        //Directorio publico
-        this.app.use(express.static('public'));
+    //Lestura y Parceo del body
+    this.app.use(express.json());
+  }
 
-        //CORS
-        this.app.use(cors());
+  routes() {
+    this.app.use(this.authPath, require("../routes/auth"));
+    this.app.use(this.usuariosPath, require("../routes/usuarios"));
+  }
 
-        //Lestura y Parceo del body
-        this.app.use(express.json());
-    }
-
-    routes() {
-        this.app.use(this.authPath, require('../routes/auth'));
-        this.app.use(this.usuariosPath, require('../routes/usuarios'));
-    }
-
-    listen() {
-        this.app.listen(this.port, () => {
-            console.log('Servidor corriendo en puerto: ', this.port)
-        });
-    }
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor corriendo en puerto: ", this.port);
+    });
+  }
 }
 
 module.exports = Server;
